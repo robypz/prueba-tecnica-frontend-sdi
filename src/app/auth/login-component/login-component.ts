cd89a00c984475b80e86878ad9bbe7c696057009
Robert Yepez <robertyepez0208@hotmail.com>
2025-10-23 20:16:19 +0200

    feat: Add login functionality and update user model
    This commit introduces the login functionality, allowing users to authenticate.
    It also includes updates to the user model to support the new authentication features.

--- src/app/auth/login-component/login-component.ts
+++ src/app/auth/login-component/login-component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../shared/auth-service';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss'
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private _user = computed(() => this.authService.user());
 
+
+  public get user() : User {
+    return this._user() as User;
+  }
+
+  constructor(){
+    effect(() => {
+      console.log(this.user);
+    })
+  }
+
+
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit(){
+    this.authService.login(this.loginForm.value as User);
+  }
 }
--- src/app/auth/shared/auth-service.ts
+++ src/app/auth/shared/auth-service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private _user = signal<User | null>(null);
+
+
+  public get user(): WritableSignal<User | null> {
+    return this._user;
+  }
+
+  public login(user: User): void {
+    if (user.email.endsWith("sdi.es")) {
+      user.role = "admin";
+      user.name = "Admin";
+      this.user.set(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("token", crypto.randomUUID());
+    } else {
+      user.role = "user";
+      user.name = "User";
+      this.user.set(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("token", crypto.randomUUID());
+    }
+  }
+
+  public logout(): void {
+    this.user.set(null);
+  }
+
 }
