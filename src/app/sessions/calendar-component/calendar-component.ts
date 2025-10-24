4e5c0e851f8239d6554303d9e524ec4561fdfdd7
Robert Yepez <robertyepez0208@hotmail.com>
2025-10-24 11:08:02 +0200

    feat: Add custom date formatter for calendar component

--- src/app/sessions/calendar-component/calendar-component.ts
+++ src/app/sessions/calendar-component/calendar-component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, inject } from '@angular/core';
+import { Component, computed, effect, inject, DOCUMENT } from '@angular/core';
 import { MenuComponent } from "../../shared/components/menu-component/menu-component";
 import { SessionService } from '../shared/session-service';
 import { SessionCreateComponent } from "../session-create-component/session-create-component";
@@ -12,10 +12,16 @@ import {
   CalendarWeekViewComponent,
   CalendarDayViewComponent, CalendarEvent,
   CalendarView,
-  CalendarDatePipe
-} from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+  CalendarDatePipe,
+  CalendarDateFormatter
+} from 'angular-calendar'
+import {
+  isSameDay,
+  isSameMonth,
+} from 'date-fns';;
+import { adapterFactory, } from 'angular-calendar/date-adapters/date-fns';
 import { Session } from '../shared/session.model';
+import { CustomDateFormatter } from './custom-date-formatter.privider';
 
 @Component({
   selector: 'app-calendar-component',
@@ -33,7 +39,12 @@ import { Session } from '../shared/session.model';
     provideCalendar({
       provide: DateAdapter,
       useFactory: adapterFactory,
-    }),
+    }, {
+      dateFormatter: {
+        provide: CalendarDateFormatter,
+        useClass: CustomDateFormatter,
+      },
+    },),
   ],
   templateUrl: './calendar-component.html',
   styleUrl: './calendar-component.scss'
@@ -56,6 +67,7 @@ export class CalendarComponent {
   viewDate = new Date();
 
   view: CalendarView = CalendarView.Month;
+  locale: string = 'es';
   //map sessions on a array of calendarevents
   events: CalendarEvent[] = this.sessions.map(
     (session: Session) => {
@@ -66,15 +78,39 @@ export class CalendarComponent {
           primary: '#1e90ff',
           secondary: '#D1E8FF',
         },
+        actions: [
+        {
+          label: '<i class="fas fa-fw fa-trash-alt"></i>',
+          onClick: ({ event }: { event: CalendarEvent }): void => {
+            this.events = this.events.filter((iEvent) => iEvent !== event);
+            console.log('Event deleted', event);
+          },
+        },
+      ],
       };
     }
   )
+  activeDayIsOpen: boolean = true;
 
   setView(view: CalendarView) {
     this.view = view;
   }
 
   closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
 
+    dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
   }
 }
