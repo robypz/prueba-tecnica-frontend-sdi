0b0d4cb7e5c2946560f6813f2d9deb3fe9391a79
Robert Yepez <robertyepez0208@hotmail.com>
2025-10-24 10:22:23 +0200

    feat: Add calendar view and styles

--- src/app/sessions/calendar-component/calendar-component.ts
+++ src/app/sessions/calendar-component/calendar-component.ts
@@ -2,10 +2,39 @@ import { Component, computed, effect, inject } from '@angular/core';
 import { MenuComponent } from "../../shared/components/menu-component/menu-component";
 import { SessionService } from '../shared/session-service';
 import { SessionCreateComponent } from "../session-create-component/session-create-component";
+import {
+  DateAdapter,
+  provideCalendar,
+  CalendarPreviousViewDirective,
+  CalendarTodayDirective,
+  CalendarNextViewDirective,
+  CalendarMonthViewComponent,
+  CalendarWeekViewComponent,
+  CalendarDayViewComponent, CalendarEvent,
+  CalendarView,
+  CalendarDatePipe
+} from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { Session } from '../shared/session.model';
 
 @Component({
   selector: 'app-calendar-component',
-  imports: [MenuComponent, SessionCreateComponent],
+  imports: [MenuComponent,
+    SessionCreateComponent,
+    CalendarPreviousViewDirective,
+    CalendarTodayDirective,
+    CalendarNextViewDirective,
+    CalendarMonthViewComponent,
+    CalendarWeekViewComponent,
+    CalendarDayViewComponent,
+    CalendarDatePipe
+  ],
+  providers: [
+    provideCalendar({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
   templateUrl: './calendar-component.html',
   styleUrl: './calendar-component.scss'
 })
@@ -17,9 +46,35 @@ export class CalendarComponent {
     return this._sessions();
   }
 
-  constructor(){
+  constructor() {
     effect(() => {
       console.log(this.sessions);
     });
+  }
+
+  readonly CalendarView = CalendarView;
+  viewDate = new Date();
+
+  view: CalendarView = CalendarView.Month;
+  //map sessions on a array of calendarevents
+  events: CalendarEvent[] = this.sessions.map(
+    (session: Session) => {
+      return {
+        start: new Date(session.date),
+        title: session.title,
+        color: {
+          primary: '#1e90ff',
+          secondary: '#D1E8FF',
+        },
+      };
+    }
+  )
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  closeOpenMonthViewDay() {
+
   }
 }
